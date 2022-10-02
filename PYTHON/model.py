764d6b256c42ef119b94b536b64012cefe2470d3
piyushkarmhe <karmhepiyush@gmail.com>
2022-10-02 18:04:58 +0530

    Model.py STDOUT

--- PYTHON/model.py
+++ PYTHON/model.py
@@ -1,8 +1,23 @@
-import numpy as np
+import sys, json
 import os
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+blockPrint()
+
+import numpy as np
 import pickle
 from tensorflow import keras
 
+labels = ['rice' ,'maize' ,'chickpea' ,'kidneybeans' ,'pigeonpeas' ,'mothbeans'
+ ,'mungbean' ,'blackgram' ,'lentil' ,'pomegranate' ,'banana' ,'mango' ,'grapes'
+ ,'watermelon' ,'muskmelon' ,'apple' ,'orange' ,'papaya' ,'coconut' ,'cotton'
+ ,'jute' ,'coffee']
 
 dirname = os.path.dirname(__file__)
 
@@ -13,13 +28,12 @@ filename = os.path.join(dirname, '../ML/scaler.pickle')
 with open(filename, 'rb') as handle:
     scaler = pickle.load(handle)
 
-labels = ['rice' ,'maize' ,'chickpea' ,'kidneybeans' ,'pigeonpeas' ,'mothbeans'
- ,'mungbean' ,'blackgram' ,'lentil' ,'pomegranate' ,'banana' ,'mango' ,'grapes'
- ,'watermelon' ,'muskmelon' ,'apple' ,'orange' ,'papaya' ,'coconut' ,'cotton'
- ,'jute' ,'coffee']
+# data = json.loads(sys.argv[1])  
+# data = data['params'] 
+data = [30,28,30,32,52,5,98]
 
-test_1 = scaler.transform(np.array([[30,28,30,32,52,5,98]]))
-prediction = np.argmax(model.predict(test_1))
+test = scaler.transform(np.array([data]))
+prediction = np.argmax(model.predict(test))
+enablePrint()
+
 print(labels[prediction])
-
-# print(labels)
