ac83db82de00b9ae8acb128152535eb171d3f969
piyushkarmhe <karmhepiyush@gmail.com>
2022-10-03 00:20:55 +0530

    model api

--- index.js
+++ index.js
@@ -33,6 +33,37 @@ app.get(`/monitoring.html`,(req,res)=>{
     res.sendFile(__dirname+'/WEB/html/monitoring.html');
 });
 
+app.post(`/manual.html/pred`,(req,res)=>{
+
+    res.set({
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Methods': "OPTIONS,POST,GET",
+        'Access-Control-Allow-Origin': "*"
+    });
+
+    // const data = {
+    //     "params" : [30,28,30,32,52,5,98]
+    // }
+
+    const data = req.body
+    console.log(res.getHeaders());
+
+    let stringifiedData = JSON.stringify(data);
+
+    const py = spawn('python', ['PYTHON/model.py', stringifiedData]);
+
+    resultString = '';
+    py.stdout.on('data', function (stdData) {
+        resultString += stdData.toString();
+        // console.log(resultString);
+    });
+
+    py.stdout.on('end', function () {
+        console.log(resultString);
+        return res.send(resultString);
+    });
+});
+
 app.listen(8000,()=>{
     console.log(`Server Started at port : ${8000}`);
 });
